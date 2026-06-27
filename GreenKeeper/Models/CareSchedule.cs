68a3c166f6297df024a49ecfbdce65b0f50640c6
nosgard <niklas.ketzler@gmail.com>
2026-06-27 16:11:25 +0200

    ~ Added the first models including the plant, the care type and the care schedule

---
+++ GreenKeeper/Models/CareSchedule.cs
@@ -0,0 +1,21 @@
+﻿using GreenKeeper.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenKeeper.Models
+{
+    public class CareSchedule
+    {
+        public int Id { get; set; }
+        public int PlantId { get; set; }
+        public CareType Care { get; set; }
+        public DateTime? LastCaredAt { get; set; }
+        public DateTime? NextDueAt { get; set; }
+
+        // Navigation property
+        public Plant SelectedPlant { get; set; } = null!;
+        }
+}
---
+++ GreenKeeper/Models/Plant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenKeeper.Models
+{
+    public class Plant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? ImagePath { get; set; }
+        public string? Notes { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        // Navigation properties
+
+
+    }
+}
