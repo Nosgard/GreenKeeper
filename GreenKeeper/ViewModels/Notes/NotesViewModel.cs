38faec3d4003a948f19e08cecf7550c7c20cf6df
nosgard <niklas.ketzler@gmail.com>
2026-07-04 14:01:52 +0200

    ~ Created a new button to open the notes of a plant and created a new view model alongside a view for the notes to implement the presentation of notes in a separate window

---
+++ GreenKeeper/ViewModels/Notes/NotesViewModel.cs
@@ -0,0 +1,41 @@
+﻿using GreenKeeper.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenKeeper.ViewModels.Notes
+{
+    public class NotesViewModel : INotifyPropertyChanged
+    {
+        private readonly Plant _plant;
+
+        public NotesViewModel(Plant plant)
+        {
+            _plant = plant;
+        }
+
+        public string PlantName => _plant.Name;
+
+        // Extract the Notes-property from the Plant-Model to present it in the related view
+        // Currently only eligible for dummy-objects. Implementation for EF-Core will follow
+        public string Notes
+        {
+            get => _plant.Notes ?? string.Empty;
+            set
+            {
+                _plant.Notes = value;
+                OnPropertyChanged(nameof(Notes));
+            }
+        }
+
+        // Implementation of INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ GreenKeeper/Views/Notes/NotesView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using GreenKeeper.Models;
+using GreenKeeper.ViewModels.Notes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GreenKeeper.Views.Notes
+{
+    /// <summary>
+    /// Interaction logic for NotesView.xaml
+    /// </summary>
+    public partial class NotesView : Window
+    {
+        public NotesView(Plant plant)
+        {
+            InitializeComponent();
+            this.DataContext = new NotesViewModel(plant);
+        }
+    }
+}
