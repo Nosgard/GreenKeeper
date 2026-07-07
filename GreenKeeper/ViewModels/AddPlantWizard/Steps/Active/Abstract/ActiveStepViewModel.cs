1e9bdb6188c61a9e07ccc84adf17fb7681b51aeb
nosgard <niklas.ketzler@gmail.com>
2026-07-07 13:48:15 +0200

    ~ Created a new base view model for the steps in the Wizard to prepare the implementation of the Fertilizing-Step that is optional. This will make sure that the Next-Button will either be shown as "Next" or "Skip" depending on whether the step is mandatory or optional. Also replaced the view models in appropriate folders

---
+++ GreenKeeper/ViewModels/AddPlantWizard/Steps/Active/Abstract/ActiveStepViewModel.cs
@@ -0,0 +1,84 @@
+﻿using GreenKeeper.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenKeeper.ViewModels.AddPlantWizard.Steps.Active
+{
+    public abstract class ActiveStepViewModel
+    {
+        private string _amountText = string.Empty;
+
+        public string AmountText
+        {
+            get => _amountText;
+            set
+            {
+                if (_amountText == value)
+                {
+                    return;
+                }
+                _amountText = value;
+                OnPropertyChanged(nameof(AmountText));
+                OnPropertyChanged(nameof(CanProceed));
+            }
+        }
+
+        private TimeUnit _selectedUnit = TimeUnit.Days;
+        public TimeUnit SelectedUnit
+        {
+            get => _selectedUnit;
+            set
+            {
+                if (_selectedUnit == value)
+                {
+                    return;
+                }
+                _selectedUnit = value;
+                OnPropertyChanged(nameof(SelectedUnit));
+            }
+        }
+
+        /// <summary>
+        /// Options for the ComboBox
+        /// 
+        /// Key: Actual Enum-Value (will be bound)
+        /// Value: Text that the user sees
+        /// </summary>
+        public IReadOnlyList<KeyValuePair<TimeUnit, string>> AvailableUnits { get; } =
+            new List<KeyValuePair<TimeUnit, string>>
+            {
+                new(TimeUnit.Hours, "Hours"),
+                new(TimeUnit.Days, "Days"),
+                new(TimeUnit.Weeks, "Weeks"),
+                new(TimeUnit.Months, "Months"),
+                new(TimeUnit.Years, "Years"),
+            };
+
+        /// <summary>
+        /// The amount needs to have a positive number.
+        /// In the context of watering it has the same meaning as CanProceed (mandatory).
+        /// Fertilizing is optional is used as a help value (for more info go to NextButtonLabel)
+        /// </summary>
+        protected bool HasValidAmount => int.TryParse(AmountText, out int amount) && amount > 0;
+
+        // Mandatory for watering + optional for fertilizing.
+        // Will be implemented by the affected class
+        public abstract bool CanProceed { get; }
+
+        // Standard: "Next"
+        // In the context of fertilizing: "Skip" or "Next"
+        public virtual string NextButtonLabel => "Next";
+
+
+        // Implementation of INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- GreenKeeper/ViewModels/AddPlantWizard/Steps/Base/IWizardStepViewModel.cs
+++ GreenKeeper/ViewModels/AddPlantWizard/Steps/Base/IWizardStepViewModel.cs
@@ -11,5 +11,9 @@ namespace GreenKeeper.ViewModels.AddPlantWizard.Steps
     {
         // Controls the "Next"-Button on every page whether to be active or not
         bool CanProceed { get; }
+
+        // Controls the text of the "Next"-Button.
+        // Usually "Next", in case of optional and empty steps "Skip"
+        string NextButtonLabel { get; }
     }
 }
