78f24fe35d74661c49dc548b6b847b8221596fd6
nosgard <niklas.ketzler@gmail.com>
2026-07-28 13:23:17 +0200

    ~ Created a Smoke-Test to check if the test project is setup correctly

---
+++ GreenKeeper.Tests/SmokeTest.cs
@@ -0,0 +1,18 @@
+﻿using GreenKeeper.Converters;
+
+namespace GreenKeeper.Tests
+{
+    public class SmokeTest
+    {
+        // Simple test that confirms the correct setup of the test project
+        [Fact]
+        public void ProjectSetup_CompilesAndReferencesMainProject()
+        {
+            // Pure access test.
+            // In case the compilation fails, the ProjectReference on GreenKeeper.csproj doesn't work
+            var result = TimeUnitConverter.ToDueDateText(null);
+
+            Assert.Equal(string.Empty, result);
+        }
+    }
+}
