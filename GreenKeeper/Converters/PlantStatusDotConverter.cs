46aa1abda67e7d58b2a90221869f05d3c5604adf
nosgard <niklas.ketzler@gmail.com>
2026-08-06 15:55:29 +0200

    ~ Created a new converter that determines the Dot-Icon depending on the due date of one or two active Care-Schedules and integrated it in the App.xaml as well as the MainWindow.xaml where a new column for the dots in the template for the ListViewItems is set up

---
+++ GreenKeeper/Converters/PlantStatusDotConverter.cs
@@ -0,0 +1,55 @@
+﻿using GreenKeeper.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GreenKeeper.Converters
+{
+
+    /// <summary>
+    /// Determines which status dot icon to show next to a plant in the
+    /// sidebar, based on the most urgent state among its Watering and
+    /// Fertilizing schedules (Sunlight is irrelevant, since it has no
+    /// due date). Overdue takes priority over "due today", which in turn
+    /// takes priority over the default green state
+    /// </summary>
+    public class PlantStatusDotConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not Plant plant)
+            {
+                return "/Resources/Icons/Dots/GreenDot.png";
+            }
+
+            var relevantSchedules = plant.CareSchedules
+                .Where(s => s.Care == Models.Enums.CareType.Water || s.Care == Models.Enums.CareType.Nutrients)
+                .Where(s => s.NextDueAt.HasValue);
+
+            var today = DateTime.Now.Date;
+
+            bool anyOverdue = relevantSchedules.Any(s => s.NextDueAt!.Value.Date < today);
+            if (anyOverdue)
+            {
+                return "/Resources/Icons/Dots/RedDot.png";
+            }
+
+            bool anyDueToday = relevantSchedules.Any(s => s.NextDueAt!.Value.Date == today);
+            if (anyDueToday)
+            {
+                return "/Resources/Icons/Dots/YellowDot.png";
+            }
+
+            return "/Resources/Icons/Dots/GreenDot.png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
