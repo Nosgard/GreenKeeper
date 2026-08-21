83010a4d5114f0b65e1b59653b29edd45826d2a2
nosgard <niklas.ketzler@gmail.com>
2026-08-21 14:50:40 +0200

    ~ All enum values of CareType are now numbered since EF Core persists enums as an integer. This will make sure that the enum values always stay in the right order

--- GreenKeeper/Models/Enums/CareType.cs
+++ GreenKeeper/Models/Enums/CareType.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace GreenKeeper.Models.Enums
 {
+    /// <summary>
+    /// All possible types of Care-Schedules. The values have numberings because EF Core stores
+    /// enums as an integer - those numbers are part of the persisted data format.
+    /// 
+    /// Warning:
+    /// Reordering or renumbering members would silently re-map existing user recors to the wrong
+    /// care type. Renaming a member is safe, as long as its number stays the same.
+    /// </summary>
     public enum CareType
     {
-        Watering,
-        Fertilizing,
-        Sunlight
+        Watering = 0,
+        Fertilizing = 1,
+        Sunlight = 2
     }
 }
